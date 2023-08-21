9b4527b6c821dddd9dcea092023d9ec7ca9244d4
vishal_ aitha <vishalaithaprod@gmail.com>
2023-08-22 02:10:00 +0530

    ver 1.1.3

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,25 @@
 import User from "../models/user.js";
 import Product from "../models/products.js";
+import gst from "../controllers/tax.js";
+
+
+// Show menu functionality, THIS IS ONLY FUNCTION WHICH ANYONE CAN USE WITHOUT LOGIN CREDENTIALS
+
+const showMenu = async (req, res) => {
+  const products = await Product.find({});
+  const listwithprices = [];
+  for (let i = 0; i < products.length; i++) {
+    listwithprices.push(
+      "Product id " +
+        products[i].productId +
+        " | Product name: " +
+        products[i].productName +
+        " | Product Price: " +
+        products[i].productPrice
+    );
+  }
+  res.send(listwithprices);
+};
 
 // Sign Up functionality
 
@@ -37,13 +57,14 @@ const loginUser = async (req, res) => {
 
 const authorization = async (req, res) => {
   const body = req.body;
-  const user = await User.findOne({ username: req.params.id });
+  const user = await User.findOne({username:body.username});
   if (!user) {
-    return false;
-  } else if (user.password == body.password) {
-    return true;
+    // console.log('here');
+    return -1;
+  } else if (user != null && user.password == body.password) {
+    return 1;
   } else {
-    return false;
+    return 0;
   }
 };
 
@@ -51,10 +72,12 @@ const authorization = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   // this can be used to get all the users from the database but the access is only for admin
+
   const body = req.body;
   if (body.password == "admin123" && body.username == "admin") {
     const users = await User.find({});
-    res.send(users);
+    // init();
+    res.send({ msg: "All the products are updated ", users });
   } else {
     res.send("You are not authorized to access this page");
   }
@@ -65,29 +88,61 @@ const getAllUsers = async (req, res) => {
 const getCart = async (req, res) => {
   const body = req.body;
   const user = await User.findOne({ username: req.params.id });
-
-  if (authorization(req, res)) {
+  const result = await authorization(req, res);
+  if (result == 1) {
     const cart = user.cart;
     res.send(cart);
+  } else if (result == -1) {
+    res.send({ message: "You are not a valid user" });
   } else {
-    res.send("You are not authorized to access this page");
+    res.send({ message: "Password entered is incorrect" });
+  }
+};
+
+// Delete for a product
+
+const removefromcart = async (req, res) => {
+  const user = await User.findOne({ username: req.params.id });
+  const result = await authorization(req, res);
+  if (result == 1) {
+    if (user.cart.includes(req.body.pid)) {
+      const cart = user.cart;
+      cart.pop(req.body.pid);
+      await user.save();
+      const cartupdated = user.cart;
+      res.send({
+        message: "Successfully removed from cart and your current cart is ",
+        data: cartupdated,
+      });
+    } else {
+      res.send({ message: "Product is not in the cart" });
+    }
+  } else if (result == -1) {
+    res.send({ message: "You are not a valid user" });
+  } else {
+    res.send({ message: "Password entered is incorrect" });
   }
 };
 
 // Add cart for a specific user functionality
 
 const addtocart = async (req, res) => {
-
   const body = req.body;
   const user = await User.findOne({ username: req.params.id });
-
-  if (authorization(req,res)) {
+  const result = await authorization(req, res);
+  if (result == 1) {
     const cart = user.cart;
-    user.cart.push(req.body.pid);
-    await user.save();
-    res.send({ message: "Successfully added to cart", data: user.cart });
+    if (cart.includes(req.body.pid)) {
+      user.cart.push(req.body.pid);
+      await user.save();
+      res.send({ message: "Successfully added to cart", data: user.cart });
+    } else {
+      res.send((message = "Product is not available in the menu"));
+    }
+  } else if (result == -1) {
+    res.send({ message: "You are not a valid user" });
   } else {
-    res.send("You are not authorized to access this page");
+    res.send({ message: "Password entered is incorrect" });
   }
 };
 
@@ -95,31 +150,79 @@ const addtocart = async (req, res) => {
 
 const getbill = async (req, res) => {
   const user = await User.findOne({ username: req.params.id });
-  if (authorization(req,res)) {
+  const result = await authorization(req, res);
+  // console.log("result is " + result+" "+user);
+  if (result == 1) {
     let bill = 0;
     const cart = user.cart;
+    const dataofproducts = [];
     for (let i = 0; i < cart.length; i++) {
       const product = await Product.findOne({ productId: cart[i] });
-      bill += product.productPrice;
-     }
-    res.send("Bill is" + bill);
+      const taxapplied = product.productPrice + gst(product);
+      const qty = cart.filter((x) => x == cart[i]).length;
+      dataofproducts.push(
+        "\nProduct Name: " +
+          product.productName +
+          " || Product Price: " +
+          product.productPrice +
+          " || Quantity: " +
+          qty +
+          " || Before Tax " +
+          qty * product.productPrice +
+          " || After Tax: " +
+          qty * taxapplied
+      );
+      bill += taxapplied;
+    }
+    const productsset = new Set(dataofproducts);
+    res.send("Total Bill value is " + bill + "\n" + Array.from(productsset));
+  } else if (result == -1) {
+    res.send({ message: "You are not a valid user" });
   } else {
-    res.send("You are not authorized to access this page");
+    res.send("You're password is incorrect");
   }
 };
 
-// Show menu functionality
+// TAX CALCULATION IS DONE IN TAX.JS FILE
 
-const showMenu = async (req, res) => {
-  const products = await Product.find({});
-  const listwithprices = [];
-  for (let i = 0; i < products.length; i++) {
-    listwithprices.push(
-      products[i].productName + " " + products[i].productPrice
-    );
-  }
-  res.send(listwithprices);
-};
+
+// This is only used when admin wants to add products to the database and if we call it once it will add evreything to the database
+
+// const init = async () => {
+//   const products = [
+//     {
+//       productName: "Ice Cream - Strawberry",
+//       productPrice: 9536,
+//       productId: 1423,
+//     },
+//     {
+//       productName: "Tomatoes - Diced, Canned",
+//       productPrice: 16772,
+//       productId: 1192,
+//     },
+//     { productName: "Peas - Pigeon, Dry", productPrice: 8762, productId: 1001 },
+//     { productName: "Instant Coffee", productPrice: 3952, productId: 154 },
+//     {
+//       productName: "Cheese - Pied De Vents",
+//       productPrice: 8229,
+//       productId: 652,
+//     },
+//     { productName: "Sour Puss Raspberry", productPrice: 7739, productId: 930 },
+//     { productName: "Pepsi - 600ml", productPrice: 11475, productId: 1827 },
+//     { productName: "Tea - Decaf Lipton", productPrice: 14492, productId: 478 },
+//     { productName: "Savory", productPrice: 13318, productId: 1667 },
+//     {
+//       productName: "Bread Crumbs - Japanese Style",
+//       productPrice: 12380,
+//       productId: 240,
+//     },
+//   ];
+//   products.forEach(async (p) => {
+//     let product = new Product(p);
+//     await product.save();
+//   });
+// };
+
 export {
   signupUser,
   loginUser,
@@ -128,4 +231,5 @@ export {
   addtocart,
   getbill,
   showMenu,
+  removefromcart,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { signupUser,loginUser,getAllUsers,getCart,addtocart,getbill,showMenu} from "../controllers/auth.js";
+import { signupUser,loginUser,getAllUsers,getCart,addtocart,getbill,showMenu, removefromcart} from "../controllers/auth.js";
 
 // these are the routes that are using get methods
 
-router.get("/getallusers", getAllUsers);
 
 router.get("/showmenu",showMenu)
 
 // these are the routes that are used in post methods
+
+router.post("/getallusers", getAllUsers);
 
 router.post("/:id/getbill",getbill)
 
@@ -20,6 +21,10 @@ router.post("/login", loginUser);
 
 router.post("/:id/addtocart",addtocart);
 
+
+// these are all the routes that are used in delete methods
+
+router.delete("/:id/removefromcart",removefromcart)
 // router.post("/:id/removefromcart",removefromcart);
 
 export default router;
